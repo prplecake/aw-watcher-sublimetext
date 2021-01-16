f7be57eaf1b07c790a0364b2acf59f9221ea7125
Matthew Jorgensen <matthew@jrgnsn.net>
2021-01-15 23:00:04 -0600

    Initial commit

---
+++ activitywatch/api.py
@@ -0,0 +1,111 @@
+import json
+import socket
+import requests
+from datetime import datetime
+
+from . import utils
+
+
+class ActivityWatchAPI(object):
+	_last_heartbeat = datetime.now()
+	debug = False
+	url = None
+
+	def __init__(self):
+		utils.log("ActivityWatchAPI initializing")
+
+	def setup(self, client_id, host, port, heartbeat_freq):
+		self.url = "http://{}:{}".format(host, port)
+		self.client_id = client_id
+		self.hostname = socket.gethostname()
+		self.freq = heartbeat_freq
+
+	def enable_debugging(self):
+		self.debug = True
+		utils.log("API debugging enabled.")
+
+	def _make_url(self, endpoint):
+		return "{}/api/0/buckets/{}".format(self.url, endpoint)
+
+	def _rate_limited(self, now):
+		return (now - self._last_heartbeat).total_seconds() > self.freq
+
+	def check(self):
+		if self.debug:
+			utils.log("Checking server connection")
+		headers = {"Content-type": "application/json"}
+		try:
+			requests.get(self._make_url(""), headers=headers)
+			self.connected = True
+		except requests.RequestException:
+			self.connected = False
+			utils.log("could not connect\n\turl: {}".format(self.url))
+
+		return self.connected
+
+	def ensure_bucket(self, bucket_id):
+		if self.debug:
+			utils.log("Ensuring bucket exists.")
+		bucket = self.get_bucket(bucket_id)
+		bucket_exists = 'id' in bucket
+		if not bucket_exists:
+			self.create_bucket(bucket_id)
+
+	def get_bucket(self, bucket_id):
+		if self.debug:
+			utils.log("Retrieving bucket.")
+		endpoint = "{}".format(bucket_id)
+		headers = {"Content-type": "application/json"}
+		resp = requests.get(self._make_url(endpoint), headers=headers)
+		return json.loads(resp.text)
+
+	def create_bucket(self, bucket_id):
+		if self.debug:
+			utils.log("Creating bucket.")
+		endpoint = "{}".format(bucket_id)
+		data = {
+			"client": self.client_id,
+			"type": 'app.editor.activity',
+			"hostname": self.hostname,
+		}
+		headers = {"Content-type": "application/json"}
+		resp = requests.post(
+			self._make_url(endpoint),
+			data=json.dumps(data), headers=headers)
+		return json.loads(resp.text)
+
+	def delete_bucket(self, bucket_id):
+		if self.debug:
+			utils.log("Deleting bucket.")
+		endpoint = "{}?force=1".format(bucket_id)
+		resp = requests.delete(self._make_url(endpoint))
+		return json.loads(resp.text)
+
+	def heartbeat(self, bucket_id, event_data, pulsetime=30):
+		now = datetime.utcnow()
+		if self.debug:
+			utils.log("now: {}\n\tnow.isoformat(): {}".format(
+				now, now.isoformat()))
+
+		if not self._rate_limited(now):
+			return
+
+		if self.debug:
+			utils.log("Heartbeat")
+			utils.log("now: {}".format(now))
+
+		endpoint = "{}/heartbeat?pulsetime={}".format(
+			bucket_id, pulsetime)
+
+		data = {
+			"timestamp": now.isoformat(),
+			'duration': 0,
+			'data': event_data,
+		}
+		headers = {"Content-type": "application/json"}
+		resp = requests.post(
+			self._make_url(endpoint),
+			data=json.dumps(data),
+			headers=headers)
+		self._last_heartbeat = now
+		return resp
---
+++ main.py
@@ -0,0 +1,105 @@
+import sublime
+import sublime_plugin
+
+from .activitywatch.api import ActivityWatchAPI
+from .activitywatch import utils
+
+# globals
+CLIENT_ID = "aw-watcher-sublimetext"
+SETTINGS_FILE = "aw-watcher-sublimetext.sublime-settings"
+SETTINGS = {}
+DEBUG = False
+CONNECTED = False
+api = ActivityWatchAPI()
+
+
+def plugin_loaded():
+	global SETTINGS
+	SETTINGS = sublime.load_settings(SETTINGS_FILE)
+
+	utils.log("Initializing ActivityWatch plugin.")
+
+	after_loaded()
+
+
+def after_loaded():
+	global CONNECTED, DEBUG
+	if DEBUG:
+		utils.log("after_loaded() called")
+	global api
+	api.setup(
+		CLIENT_ID, SETTINGS.get("hostname"),
+		SETTINGS.get("port"), SETTINGS.get("heartbeat_frequency"))
+	CONNECTED = api.check()
+	DEBUG = SETTINGS.get("debug")
+	if DEBUG:
+		api.enable_debugging()
+		utils.log("Connected? {}".format(CONNECTED))
+
+
+def get_file_name(view):
+	return view.file_name() or view.name() or "untitled"
+
+
+def get_project_name(view):
+	window = view.window()
+	project = "unknown"
+	if hasattr(window, "project_data"):
+		project = window.project_data()
+	if "name" in project:
+		project = project.get("name")
+	elif "folders" in project:
+		for folder in project.get("folders"):
+			if get_file_name(view).startswith(folder.get("path")):
+				project = folder.get("path")
+				break
+	return project
+
+
+def get_language(view):
+	point = view.sel()[0].begin()
+	scopes = view.scope_name(point).strip().split(" ")
+	return scopes[0]
+
+
+def handle_activity(view):
+	if DEBUG:
+		utils.log("handle_activity() fired")
+	if CONNECTED:
+		api.ensure_bucket(SETTINGS.get("bucket_name"))
+	else:
+		active_window = sublime.active_window()
+		if active_window:
+			for view in active_window.views():
+				view.set_status(
+					CLIENT_ID,
+					"[aw-watcher-sublimetext] Could not connect "
+					"to aw-server")
+	event_data = {
+		"file": get_file_name(view),
+		"project": get_project_name(view),
+		"language": get_language(view),
+	}
+	api.heartbeat(SETTINGS.get("bucket_name"), event_data)
+
+
+if DEBUG:
+	utils.log("hostname: {}\n\tport: {}\n\theartbeat_freq: {}\n\tbucket_name: {}\n\tdebug: {}".format(
+		SETTINGS.get("hostname"), SETTINGS.get("port"),
+		SETTINGS.get("heartbeat_freq"), SETTINGS.get("bucket_name"),
+		SETTINGS.get("debug")))
+
+
+class ActivityWatchListener(sublime_plugin.EventListener):
+
+	def on_selection_modified_async(self, view):
+		if DEBUG:
+			utils.log("on_selection_modified_async fired")
+		if CONNECTED:
+			handle_activity(view)
+
+	def on_modified_async(self, view):
+		if DEBUG:
+			utils.log("on_modified_async fired")
+		if CONNECTED:
+			handle_activity(view)
